6ac60c303212196149dda97447f115a37a4b0605
Philip D'Arcy <heavyweaponsguy96@gmail.com>
2018-09-17 14:00:23 +0100

    starting work

---
+++ SecureDevApp/SecureDevApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SecureDevApp
+{
+    class Program
+    {
+        public static string name;
+        static void Main(string[] args)
+        {
+            string[] lines;
+            Console.WriteLine("WHAT UP BITCHES IT'S YA BOY....");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}!! YEAH THAT'S WHO I WAS THINKING OF!!",name);
+            Console.ReadKey();
+            if (name != "DELETE")
+            {
+                try
+                {
+                    lines = File.ReadAllLines(@"..\..\test.txt");
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine("OOPS");
+                    lines = null;
+                }
+
+                if (lines != null)
+                    foreach (string s in lines)
+                    {
+                        Console.WriteLine(s);
+                    }
+
+                File.AppendAllText(@"..\..\test.txt", name + "\n");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Deleting File");
+                if(File.Exists(@"..\..\test.txt"))
+                File.Delete(@"..\..\test.txt");
+                Console.ReadKey();
+                
+            }
+            
+           
+           
+           
+
+
+        }
+    }
+}
